8b12ca5884662858edfdb7727d139f6d600e59d4
Diarmaid O'Keeffe <X00169640@mytudublin.ie>
2021-12-12 16:30:54 +0000

    added tfvars

---
+++ azure/variables.tf
@@ -0,0 +1,18 @@
+variable "name" {}
+variable "location" {}
+variable "vnetcidr" {}
+variable "websubnetcidr" {}
+variable "appsubnetcidr" {}
+variable "dbsubnetcidr" {}
+variable "web_host_name"{}
+variable "web_username" {}
+variable "app_host_name"{}
+variable "app_username" {}
+variable "primary_database" {}
+variable "primary_database_admin" {}
+variable "primary_database_password" {}
+variable "primary_database_version" {}
+
+
+
+
